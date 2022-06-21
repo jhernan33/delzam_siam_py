7954a39436777e311af4a61bfd9b180cb3a2a338
Hernan <jhernan33@gmail.com>
2022-06-21 11:35:06 -0400

    actualizacion en el serializador de articulo, para que muestra el codigo de la familia

--- siam/asiam/serializers/articuloSerializer.py
+++ siam/asiam/serializers/articuloSerializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from asiam.models import Articulo
+from asiam.serializers import SubFamiliaSerializer
 
 class ArticuloSerializer(serializers.ModelSerializer):
+    family = serializers.ReadOnlyField(source='codi_sufa.codi_fami.id')
     subfamilia = serializers.ReadOnlyField(source='codi_sufa.desc_sufa')
     compraPresentacion = serializers.ReadOnlyField(source='codc_pres.desc_pres')
     ventaPresentacion = serializers.ReadOnlyField(source='codv_pres.desc_pres')
@@ -11,7 +13,7 @@ class ArticuloSerializer(serializers.ModelSerializer):
         model = Articulo
         field = ('id','codi_arti','idae_arti','desc_arti','coba_arti','cmin_arti','cmax_arti'
         ,'por1_arti','por2_arti','por3_arti','ppre_arti','codi_sufa','foto_arti','exgr_arti'
-        ,'codc_pres','codv_pres','capc_arti','capv_arti','proc_arti','codi_ivti','subfamilia','compraPresentacion','ventaPresentacion','ivaValor')
+        ,'codc_pres','codv_pres','capc_arti','capv_arti','proc_arti','codi_ivti','familia','subfamilia','compraPresentacion','ventaPresentacion','ivaValor')
         exclude =['created','updated','deleted','esta_ttus']
     
         desc_arti = serializers.CharField(trim_whitespace=False)
