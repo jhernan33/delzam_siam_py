5ac84bfafe756006d73678183b43dfd3d1e9c322
Hernan <jhernan33@gmail.com>
2023-03-27 11:38:48 -0400

    update report client zone description

--- siam/asiam/models/zona.py
+++ siam/asiam/models/zona.py
@@ -19,3 +19,31 @@ class Zona(Base):
     """ Get Instance Zona """
     def getInstanceZona(Id):
         return Zona.objects.get(id = Id)
+    
+    """ Get All Zones """
+    def getZoneFilterArray(_zoneId):
+        if isinstance(_zoneId,str):
+            # Create List
+            _zoneList = []
+            ocu_pri = 0
+            # Check Count Ocurrences
+            indexes = [i for i, c in enumerate(_zoneId) if c ==',']
+            if len(indexes) >0:
+                # Iterate Indexes
+                for x in indexes:
+                    if ocu_pri == 0:
+                        _zoneList.append(int(_zoneId[ocu_pri:x]))
+                        ocu_pri = x
+                    elif ocu_pri > 0:
+                        _zoneList.append(int(_zoneId[ocu_pri+1:x]))
+                        ocu_pri = x
+                _zoneList.append(int(_zoneId[ocu_pri+1:len(_zoneId)]))
+            elif len(indexes) ==0:
+                _zoneList.append(int(_zoneId[0:len(_zoneId)]))
+
+            _result = Zona.get_queryset().filter(id__in = _zoneList).values("desc_zona")
+            all =""
+            for k in _result:
+                all += ","+k["desc_zona"] if len(all) > 1 else k["desc_zona"]
+            
+            return all
