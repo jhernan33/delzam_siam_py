503ce962a34973239160a790048745e8b2f603c7
Hernan <jhernan33@gmail.com>
2023-05-24 11:44:35 -0400

    add config https api.js, http-commonjs

--- siam/siam/settings.py
+++ siam/siam/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ['*']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
-# Add to project/settings.py
-SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
+# # Add to project/settings.py
+# SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
 # Application definition
 
@@ -215,6 +215,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://delzam.com.ve/"
     "https://delzam.com.ve/apidz/"
 ]
 
