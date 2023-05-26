897e513d584864901a7f968ebd1d2be18c0e7375
hernan <jhernan@gmail.com>
2023-05-26 07:51:31 -0400

    add change pagination

--- siam/asiam/paginations.py
+++ siam/asiam/paginations.py
@@ -1,7 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
 
 class SmallResultsSetPagination(PageNumberPagination):
     page_size = 5
     page_query_param = 'page'
     page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        response = Response(data)
+        response['count'] = self.page.paginator.count
+        response['next'] = "apidz/"+self.get_next_link()
+        response['previous'] = "apidz/"+self.get_previous_link()
+        return response
