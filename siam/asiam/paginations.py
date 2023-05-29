0dbd75927ad98e5ffef1acf122d71e9b3977d1a1
Hernan <jhernan33@gmail.com>
2023-05-28 23:51:40 -0400

    change in paginations

--- siam/asiam/paginations.py
+++ siam/asiam/paginations.py
@@ -5,20 +5,3 @@ class SmallResultsSetPagination(PageNumberPagination):
     page_size = 5
     page_query_param = 'page'
     page_size_query_param = 'page_size'
-
-    # def get_paginated_response(self, data):
-    #     # response = Response(data)
-    #     # response['count'] = self.page.paginator.count
-    #     # response['next'] = self.get_next_link()
-    #     # response['previous'] = self.get_previous_link()
-    #     # return response
-    #     return Response({
-    #         'links': {
-    #             'next': self.get_next_link(),
-    #             'previous': self.get_previous_link()
-    #         },
-    #         'count': self.page.paginator.count,
-    #         'page': int(self.request.GET.get('page', 1)), # can not set default = self.page
-    #         'page_size': int(self.request.GET.get('page_size', self.page_size)),
-    #         'results': data
-    #     })
